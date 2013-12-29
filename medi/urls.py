97a9f72f9227bac8f5339b07ac6510d7ad35c7d0
Bogdan Mihai <m.bogdan92@yahoo.com>
2013-12-29 19:16:43 +0000

    First commit. Initialized a Django project.

---
+++ medi/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'medi.views.home'),
+
+    #url(r'^upload/$', 'video_converter.views.upload'),
+    #url(r'^s3direct/', include('s3direct.urls')),
+    
+    #api urls
+    #url(r'^api/convert/$', 'video_converter.views.convert'),
+    #url(r'^api/progress/$', 'video_converter.views.progress'),
+    #url(r'^api/get_url/$', 'video_converter.views.get_url'),
+)
---
+++ medi/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.conf import settings
+
+def home(request):
+    return_dict = {}
+    t = loader.get_template('index.html')
+    c = RequestContext(request, return_dict)
+    return HttpResponse(t.render(c))
