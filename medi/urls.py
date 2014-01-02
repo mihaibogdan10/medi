5c6603248a4e1383405712b13077636b61cf582e
Bogdan Mihai <mihaibogdan10@gmail.com>
2014-01-02 15:26:11 +0000

    Created a base template with some still-ugly design

--- medi/urls.py
+++ medi/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'medi.views.home'),
+    url(r'^home$', 'medi.views.home'),
 
     #url(r'^upload/$', 'video_converter.views.upload'),
     #url(r'^s3direct/', include('s3direct.urls')),
--- medi/views.py
+++ medi/views.py
@@ -4,6 +4,6 @@ from django.conf import settings
 
 def home(request):
     return_dict = {}
-    t = loader.get_template('index.html')
+    t = loader.get_template('home.html')
     c = RequestContext(request, return_dict)
     return HttpResponse(t.render(c))
